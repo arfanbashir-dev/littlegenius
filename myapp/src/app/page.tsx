85622de3968dc1379739bad57688b56f65aac598
arfanbashir-dev <arfanbashir.dev@gmail.com>
2026-01-03 09:35:54 +0500

    update home page

--- myapp/src/app/page.tsx
+++ myapp/src/app/page.tsx
@@ -1,6 +1,38 @@
 
-export default function Page() {
-  return (
-    <h1 className="text-4xl text-red-500 font-bold">Tailwind Test</h1>
-  );
+// export default function Page() {
+//   return (
+//     <h1 className="text-4xl text-red-500 font-bold">Tailwind Test</h1>
+//   );
+// }
+
+
+import React from "react"
+
+import {
+        //  HomeVideoPage, 
+         HomeWellComePage,
+        //  HomeDetailPage, 
+        //  HomeKeyFeaturePage, 
+        //  HomeSchoolInfoPage, 
+        //  HomeFacilitiesPage, 
+        //  HomeMenuPage
+        } from "@/components"
+
+
+export default function HomePage(){   
+
+      return(
+          <div>
+
+            {/* <HomeVideoPage /> */}
+            <HomeWellComePage />
+            {/* <HomeDetailPage /> */}
+            {/* <HomeKeyFeaturePage /> */}
+            {/* <HomeSchoolInfoPage /> */}
+            {/* <HomeFacilitiesPage /> */}
+            {/* <HomeMenuPage /> */}
+
+          </div>
+      )  
+  
 }
---
+++ myapp/src/components/index.ts
@@ -0,0 +1,24 @@
+
+// import HomeVideoPage from "./HomeVideoPage"
+import HomeWellComePage from "./HomeWellcomePage"
+// import HomeDetailPage from "./HomeDetailPage"
+// import HomeKeyFeaturePage from './HomeKeyFeaturesPage'
+// import HomeSchoolInfoPage from './HomeSchoolInfoPage'
+// import HomeFacilitiesPage from './HomeFacilitiesPage'
+// import HomeMenuPage from './HomeMenuPage'
+
+
+
+export{   
+          
+    // HomeVideoPage,
+    HomeWellComePage,
+    // HomeDetailPage,
+    // HomeKeyFeaturePage,
+    // HomeSchoolInfoPage,
+    // HomeFacilitiesPage ,
+    // HomeMenuPage, 
+
+     
+}
+
